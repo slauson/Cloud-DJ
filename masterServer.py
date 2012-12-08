4579031931e3859b75e4bab705be9aeab9acfdbc
hittson <amanda.hittson@gmail.com>
2012-12-08 16:08:28 -0600

    Added ACL class functionality only.

--- masterServer.py
+++ masterServer.py
@@ -1,7 +1,5 @@
 # The master handles user authentication and passes user to data server.
 # potentially add: the ability to allow user to choose to host or listen...
-
-
 import jinja2
 import os
 import logging
@@ -15,6 +13,77 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 from dataServer import *
 
+def ACL_key(user_name=None):
+    """ Constructs a key from """
+    return db.Key.from_path('ACL', user_name or 'anonymous')
+
+def findACL(user):
+    db.get(ACL(key(user.userid()))
+#     db.GqlQuery("SELECT * "
+#                 "FROM "
+#                 "WHERE ANCESTOR IS :1", 
+#                 ACL_key(user.user_id()))
+    # Comment: 
+    # Ancestor queries, as shown here, are strongly consistent; queries that
+    # span entity groups are only eventually consistent. If we omitted the
+    # ancestor from this query, there would be a slight chance that a greeting
+    # that had just been written would not show up in a query. See example:
+    #     greetings = db.GqlQuery("SELECT * "
+    #                             "FROM Greeting "
+    #                             "WHERE ANCESTOR IS :1 "
+    #                             "ORDER BY date DESC LIMIT 10",
+    #                             guestbook_key(guestbook_name))
+
+
+class ACLEntry(db.Model):
+    """
+    Individual ACL (Access Control List) entry. 
+    Keeps track for each user who can listen to their sessions (potential listeners) 
+    and which sessions the user can listen to (potential sessions) 
+    """
+    host       = db.UserProperty()                              # User 
+    plisteners = db.ListProperty(users.User, indexed=False)     # List of users who are allowed to listen to this one 
+    psessions  = db.ListProperty(users.User, indexed=False)     # List of users whose session this user can listen to
+
+
+    def create_new(self, user):
+        host = user
+        plisteners = []
+        psessions = []
+
+    def put(self):
+        self.put()
+
+    def add(self, host, plistener):
+        """ Adds plistener to host's plistener ACL 
+        and adds host to plistener's psessions ACL.
+        For performance, allows duplicates."""
+
+        host_entry = db.get(ACL_key(host.user_id()))
+        host_entry.plisteners.append(plistener)
+        host_entry.put()
+
+        plistener_entry = db.get(ACL_key(plistener.user_id()))
+        plistener_entry.psessions.append(host)
+        plistener_entry.put()
+
+    def remove(self, host, plistener):
+        """ Removes plistener to host's plistener ACL 
+        and removes host to plistener's psessions ACL.
+        Accounts for duplicates. """
+
+        # TODO: don't put if no change?
+        host_entry = db.get(ACL_key(host.user_id()))
+        while (plistener in host_entry.plisteners):
+            host_entry.plisteners.remove(plistener.user_id())
+        host_entry.put()
+
+        plistener_entry = db.get(ACL_key(plistener.user_id()))
+        while (host in plistener_entry.psessions):
+            plistener_entry.psessions.append(host)
+        plistener_entry.put()
+
+
 class MainPage(webapp.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -24,6 +93,12 @@ class MainPage(webapp.RequestHandler):
         if not user:
             self.redirect(users.create_login_url(self.request.uri))
             return
+        
+#         ACL = findACL(user)
+#         if (ACL == None):
+#             ACL = ACLEntry()
+#             ACL.create_new()
+#             ACL.put()
 
         if not session_key:
             # No session specified, create a new one, make this the host 
